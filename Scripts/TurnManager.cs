d514fb4e87c6d9e41aab6d2edd8c8603d37a933b
Emily Braithwaite <emily@solved.net.nz>
2025-08-30 12:17:56 +1200

    Commented Player.cs and TurnManager.cs
    For #7 and #2

--- Scripts/TurnManager.cs
+++ Scripts/TurnManager.cs
@@ -1,12 +1,19 @@
 using Godot;
 using System;
-
+// -----------------------------------------------------------------------------
+// TurnManager.cs
+// Author: Emily Braithwaite
+// Purpose: Manages turn logic for the game.
+// -----------------------------------------------------------------------------
 public partial class TurnManager : Node2D
 {
 	[Export] public CharacterBody2D Player;
 	private int turnCount = 0;
 
-
+	/// <summary>
+	/// Get number of the turn the game is currently on
+	/// </summary>
+	/// <returns>turnCount</returns>
 	public int GetTurnCount()
 	{
 		return this.turnCount;
@@ -15,8 +22,11 @@ public partial class TurnManager : Node2D
 	/// <summary>
 	/// Proceed to the next turn. To be called from player script
 	/// </summary>
-	public void NextTurn()
+	/// <param name="playerAction">Action from player that has called this</param>
+	public void NextTurn(String playerAction)
 	{
-		GD.Print("Moved to next turn");
+		GD.Print("Moved to next turn. Input: " + playerAction);
+		// Increase turn count
+		turnCount++;
 	}
 }
